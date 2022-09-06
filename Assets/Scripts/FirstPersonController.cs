96a7b7c1f3b606b65624f685aad0b2519841716f
ThicosGroove <thiagottlsilva@gmail.com>
2022-09-06 17:15:05 -0300

    jump is working

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    public bool CanMove { get; private set; } = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => characterController.isGrounded && Input.GetKeyDown(jumpKey);
 
-    public bool CandMove { get; private set; } = true;
+    [Header("Fucnitonal Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
     [Header("Movement Parameters")]
-    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8f;
     [SerializeField] private float gravity = 30f;
 
     private CharacterController characterController;
@@ -21,9 +34,12 @@ public class FirstPersonController : MonoBehaviour
 
     private void Update()
     {
-        if (CandMove)
+        if (CanMove)
         {
             HandleMovementInput();
+
+            if (canJump)
+                HanldeJump();
 
             ApplyFinalMovements();
         }
@@ -31,21 +47,29 @@ public class FirstPersonController : MonoBehaviour
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((IsSprinting? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
 
         float moveDirectionY = moveDirection.y;
-        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x + (transform.TransformDirection(Vector3.right) * currentInput.y));
+        moveDirection = (transform.TransformDirection(Vector3.forward)) * currentInput.x + (transform.TransformDirection(Vector3.right)) * currentInput.y;
         moveDirection.y = moveDirectionY;
 
     }
 
 
+    private void HanldeJump()
+    {
+        if (ShouldJump)
+        {
+            moveDirection.y = jumpForce;
+        }
+    }
+
     private void ApplyFinalMovements()
     {
         if (!characterController.isGrounded)
-        {
             moveDirection.y -= gravity * Time.deltaTime;
-        }
+
+
 
         characterController.Move(moveDirection * Time.deltaTime);
     }
